7a36187c839b2c939f80027482c14c963a63b96a
Abhishek Paresh Jajal <abhijajal@gmail.com>
2019-06-08 13:08:55 -0500

    up

---
+++ Assets/MenuActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour {
+
+	public void MENU_ACTION_GotoPage(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+			Application.Quit(); 
+	}
+}
+
+
